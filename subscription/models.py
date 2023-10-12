0ba57fb32315d71b9e4e62d4a5df8adce2afe413
neeschal00 <nischal06khatri@gmail.com>
2023-10-13 00:41:35 +0545

    app and temp set up

--- subscription/models.py
+++ subscription/models.py
@@ -1,3 +1,36 @@
+from django.conf import settings
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+
+    
+
+class Plan(models.Model):
+    name = models.CharField(max_length=200)
+    
+    users = models.OneToOneField(User)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ("-created_at",)
+
+    def __str__(self):
+        return self.name
+
+
+class StripeSubscription(models.Model):
+    subscription_id = models.CharField(max_length=200)
+    customer_id = models.CharField(max_length=200)
+    price = models.IntegerField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.subscription_id
+
--- subscription/views.py
+++ subscription/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import *
+import stripe
 
 # Create your views here.
 
-def webhook_portal(request):
-    pass
+def create_subscription(request):
+
+    
+
+    checkout_session = stripe.checkout.Session.create(
+            payment_method_types=["card"],
+            
+            line_items=[
+                
+            ],
+            
+            mode="subscription",
+            success_url=settings.PAYMENT_SUCCESS_URL,
+            cancel_url=settings.PAYMENT_CANCEL_URL,
+            
+            )
+
+    return redirect(checkout_session.url)
 
 
+
+https://youtu.be/hif5eI5pBxo?si=gjiDOFRV_IemNyWV
